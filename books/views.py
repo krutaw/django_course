3f2b5849ddadc19e1c368dffbde1b854df129ad9
krutaw <andy.kruta@advisory.com>
2016-12-01 21:25:55 -0500

    Made adjustments to the list template and eliminated duplicate SQL queries

--- books/views.py
+++ books/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
 def list_books(request):
-    return render (request, "list.html")
+    """
+    List the books that have reviews
+    """
+    
+    books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+    
+    context = {
+        'books': books,
+    }
+    return render (request, "list.html", context)
+
