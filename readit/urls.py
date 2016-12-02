e6b4fd1a20549fc1df9a5c32185268d23126eaaa
krutaw <andy.kruta@advisory.com>
2016-12-01 20:41:53 -0500

    Created the initial view and url

--- readit/urls.py
+++ readit/urls.py
@@ -18,15 +18,19 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import patterns
 
+
+from books.views import list_books
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', list_books, name='books'),
 ]
 
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns += patterns(
         '',
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
+    
