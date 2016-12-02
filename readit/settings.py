be35d2ff16494f83c856393c6385b817f134af34
krutaw <andy.kruta@advisory.com>
2016-12-01 22:57:12 -0500

    Updated to include CSS and styling

--- readit/settings.py
+++ readit/settings.py
@@ -62,7 +62,7 @@ INTERNAL_IPS = ('0.0.0.0',)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'readit', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'readit', 'static'),
+)
 
 ##Django_debug_toolbar settings
 def show_toolbar(request):
